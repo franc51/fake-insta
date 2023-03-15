4a9163623e0d778db0e0cd2542c2d06dd70bc614
franc51 <101558212+franc51@users.noreply.github.com>
2023-03-15 20:50:13 +0200

    added news feed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+let respone = fetch("https://jsonplaceholder.typicode.com/todos/1");
 
-const data = json;
+then.(response => {
+  console.log()
+})
