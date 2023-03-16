d3722d441b10e71dc4a6212de0e0a94cf7b50449
franc51 <101558212+franc51@users.noreply.github.com>
2023-03-16 21:27:59 +0200

    added API

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let respone = fetch("https://jsonplaceholder.typicode.com/todos/1");
+async function getData() {
+  const response = await fetch(
+    "https://mocki.io/v1/9df2bd90-2565-45cc-ba79-f105294f0686"
+  );
+  const data = await response.json();
+  console.log(data);
 
-then.(response => {
-  console.log()
-})
+  for (let i = 0; i < data.length; i++) {
+    let story = document.getElementById("st-" + i);
+    story.innerHTML = data[i].name;
+    let story_img = document.getElementById("st-img-" + i);
+    story_img.src = data[i].photo;
+  }
+}
+getData();
